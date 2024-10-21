221a8fa77514fc65a0330354513c8d291dbde3c4
pawfs <bahriayavx@gmail.com>
2024-10-21 12:33:43 +0100

    add twig setup

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<strong>Webshop</strong>: Welcome to my shop!');
+        $petCount = 57;
+
+        $myPet = [
+          'name' => 'Pompom',
+          'species' => 'cat',
+          'age' => '4 months',
+          'status' => 'unadopted',
+        ];
+        
+        return $this->render('main/homepage.html.twig', [
+          'numberOfPets' => $petCount,
+          'myPet' => $myPet,
+        ],);
     }
 }
